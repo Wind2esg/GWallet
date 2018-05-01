48aa693796e6cb0675fae8ea242f44744b5039ce
wang guan <wind2esg@outlook.com>
2018-05-01 15:55:18 +0800

    添加项目文件。

---
+++ GWallet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GWallet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
